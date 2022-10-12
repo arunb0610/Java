6f8459463afc99b7b886cb0d63904f93df09dc7d
arunb0610 <stylewalker.ak@gmail.com>
2022-10-12 14:48:17 +0530

    Used two classes

---
+++ Customer.java
@@ -0,0 +1,8 @@
+class Customer
+{
+public static void main (String[] args)
+{
+Theatre2 theatre = new Theatre2();
+theatre.BookTicket(200);
+}
+}
---
+++ Theatre2.java
@@ -0,0 +1,7 @@
+class Theatre2
+{
+void BookTicket (int Amount)
+{
+System.out.println("Balance is : " + (Amount-120));
+}
+}
