6d4a27c8e6f13098219c497a15f353ca42b7b9fa
arunb0610 <stylewalker.ak@gmail.com>
2022-10-13 12:13:54 +0530

    Using two class

---
+++ GrandMall.java
@@ -0,0 +1,12 @@
+class GrandMall
+{
+    int TicketPrice = 120 ;
+    void show (String MovieName)
+{
+    System.out.println("Showing Movie :" + MovieName);
+}
+private void get_profit()
+{
+    System.out.println("30% Profit");
+}
+}
---
+++ Viewer.java
@@ -0,0 +1,9 @@
+class Viewer
+{
+    public static void main (String[] args)
+{
+    GrandMall manager = new GrandMall();//cretaing object by calling GrandMall class
+    manager.show("Ponniyin Selvan");
+    //manager.get_profit();
+}
+}
