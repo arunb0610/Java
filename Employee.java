e4fc8e698832d6d6f086b3b4226c4aaac1a2d902
arunb0610 <stylewalker.ak@gmail.com>
2022-10-30 08:48:16 +0530

    Using interface

---
+++ Employee.java
@@ -0,0 +1,29 @@
+class Employee implements Offerletter
+{
+
+public static void main(String[] args)
+{
+Employee arun = new Employee();
+System.out.println(Offerletter.name);
+System.out.println(Employee.salary);
+arun.wear_formal_dress();
+arun.wear_formal_dress();
+arun.wear_casual_dress();
+}
+public void work_40hours()
+{
+System.out.println("Working");
+}
+public void wear_formal_dress()
+{
+System.out.println("Wearing");
+}
+public void wear_casual_dress()
+{
+System.out.println("Wearing casual");
+}
+public void apply_leave_10days()
+{
+
+}
+}
---
+++ Offerletter.java
@@ -0,0 +1,15 @@
+interface Offerletter
+{
+
+int salary = 300000;
+String name = "MNC";
+
+public void work_40hours();
+
+public void wear_formal_dress();
+
+public void wear_casual_dress();
+
+public void apply_leave_10days();
+
+}
