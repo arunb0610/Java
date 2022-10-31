e2d80763e4c9285ad6b19c4652d342dbaf814a77
arunb0610 <stylewalker.ak@gmail.com>
2022-10-31 19:23:10 +0530

    If Else condition

---
+++ Assignment.java
@@ -0,0 +1,24 @@
+class Assignment
+{
+public static void main(String[] args)
+{
+String moviename = "Kantara";
+boolean review = true ;
+boolean subtitle = true ;
+boolean good = false ;
+boolean tamil = true ;
+
+if(review&&subtitle)
+{
+if(tamil)
+{
+System.out.println("Book ticket");
+}
+
+else
+{
+System.out.println("Don't book ticket");
+}
+}
+}
+}
