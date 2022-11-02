6eb70caf8fe782e6d981eeaaf611f820da400305
arunb0610 <stylewalker.ak@gmail.com>
2022-11-02 17:27:18 +0530

    Using if else , nested if , ifelseif

---
+++ If_else.java
@@ -0,0 +1,15 @@
+class If_else
+{  
+public static void main(String[] args)
+{  
+int number = 34;   
+    if(number%2==0)
+    {  
+        System.out.println("The given number is even number");  
+    }
+    else
+    {  
+        System.out.println("The given number is odd number");  
+    }  
+}  
+}
---
+++ If_else_if.java
@@ -0,0 +1,20 @@
+class If_else_if
+{    
+public static void main(String[] args)
+{    
+int number = -13; 
+   
+    if(number>0)
+    {  
+        System.out.println(number+" is Positive number");  
+    }
+    else if(number<0)
+    {  
+        System.out.println(number+" is Negative number");  
+    }
+    else
+    {  
+        System.out.println("The number is zero");  
+    }  
+}    
+} 
