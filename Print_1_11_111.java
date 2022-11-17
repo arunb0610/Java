8a78e56ec89ed25da25caf36c3a4534b9d2df2b1
arunb0610 <stylewalker.ak@gmail.com>
2022-11-17 14:52:33 +0530

    printing 1 11 111

---
+++ Print_1_11_111.java
@@ -0,0 +1,18 @@
+class Print_1_11_111
+{
+public static void main(String[] args)
+{
+Print_1_11_111.printing();
+}
+static void printing()
+{
+String str = "";
+int num = 1, num2 = 0;
+while(num2<5)
+{
+str += num;
+System.out.println(str);
+num2++;
+}
+}
+} 
