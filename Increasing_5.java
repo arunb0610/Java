cb5a708a36e674cbb79509818c2ce5b318cf0820
arunb0610 <stylewalker.ak@gmail.com>
2022-11-17 14:55:13 +0530

    Increasing number by 5

---
+++ Increasing_5.java
@@ -0,0 +1,21 @@
+class Increasing_5
+{
+public static void main(String[] args)
+{
+Increasing_5.find_increase();
+}
+static void find_increase()
+{
+int num = 0, num2 = 0 ;
+while(num<25)
+{
+num += 5;
+System.out.print(num+"\t"); 
+}
+while(num2<20)
+{
+num2 += 4;
+System.out.print(+num2+"\t");
+}
+}
+}
